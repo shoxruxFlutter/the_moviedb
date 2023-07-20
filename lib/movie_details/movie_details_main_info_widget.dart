a1d9a8b69a99d01db804ab3722ebb905dcff1247
Shohruh <shoxruxxasanov2017@gmail.com>
2023-07-20 12:10:35 +0500

    verstka_movie_details_main_info

--- lib/movie_details/movie_details_main_info_widget.dart
+++ lib/movie_details/movie_details_main_info_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:the_moviedb/main_screen/test.dart';
 import 'package:the_moviedb/resources/resources.dart';
 
 class MovieDetailsMainInfoWidget extends StatelessWidget {
@@ -7,14 +8,46 @@ class MovieDetailsMainInfoWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: const [
-        _TopPosterWidget(),
-        Padding(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const _TopPosterWidget(),
+        const Padding(
           padding: EdgeInsets.all(20.0),
           child: _MovieNameWidget(),
         ),
+        _ScoreWidget(),
+        const _SummaryWidget(),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: _OverviewWidget(),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: _DescriptionWidget(),
+        ),
+        SizedBox(height: 30),
+        _PeopleWidgets(),
       ],
     );
+  }
+
+  Text _DescriptionWidget() {
+    return const Text(
+        'An elite Navy SEAL uncovers an international conspiracy while seeking justice for the murder of his pregnant wife.',
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.w400,
+          fontSize: 16,
+        ));
+  }
+
+  Text _OverviewWidget() {
+    return const Text('Overview',
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.w400,
+          fontSize: 16,
+        ));
   }
 }
 
@@ -55,6 +88,131 @@ class _MovieNameWidget extends StatelessWidget {
               style: TextStyle(fontWeight: FontWeight.w400, fontSize: 16)),
         ],
       ),
+    );
+  }
+}
+
+class _ScoreWidget extends StatelessWidget {
+  const _ScoreWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        TextButton(
+          onPressed: () {},
+          child: Row(
+            children: const [
+              SizedBox(
+                width: 40,
+                height: 40,
+                child: RadialPercentWidget(
+                  percent: 0.72,
+                  fillColor: Color.fromARGB(255, 10, 23, 25),
+                  lineColor: Color.fromARGB(255, 37, 203, 103),
+                  freeColor: Color.fromARGB(255, 25, 54, 31),
+                  lineWidth: 3,
+                  child: Text('72'),
+                ),
+              ),
+              SizedBox(width: 10),
+              Text('User Score'),
+            ],
+          ),
+        ),
+        Container(width: 1, height: 15, color: Colors.grey),
+        TextButton(
+            onPressed: () {},
+            child: Row(
+              children: [Icon(Icons.play_arrow), Text('Play Trailer')],
+            )),
+      ],
+    );
+  }
+}
+
+class _SummaryWidget extends StatelessWidget {
+  const _SummaryWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ColoredBox(
+      color: Color.fromRGBO(22, 21, 25, 1.0),
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 70),
+        child: Text('R 29/04/2021 (US) 1h 49m Action, Adventure, Thriller, War',
+            maxLines: 3,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w400,
+              fontSize: 16,
+            )),
+      ),
+    );
+  }
+}
+
+class _PeopleWidgets extends StatelessWidget {
+  const _PeopleWidgets();
+
+  @override
+  Widget build(BuildContext context) {
+    const nameStyle = TextStyle(
+      color: Colors.white,
+      fontWeight: FontWeight.w400,
+      fontSize: 16,
+    );
+    const jobTitleStyle = TextStyle(
+      color: Colors.white,
+      fontWeight: FontWeight.w400,
+      fontSize: 16,
+    );
+    return Column(
+      children: [
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Stefano Sollima', style: nameStyle),
+                Text('Director', style: jobTitleStyle),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Stefano Sollima', style: nameStyle),
+                Text('Director', style: jobTitleStyle),
+              ],
+            )
+          ],
+        ),
+        const SizedBox(height: 20),
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Stefano Sollima', style: nameStyle),
+                Text('Director', style: jobTitleStyle),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Stefano Sollima', style: nameStyle),
+                Text('Director', style: jobTitleStyle),
+              ],
+            )
+          ],
+        )
+      ],
     );
   }
 }
